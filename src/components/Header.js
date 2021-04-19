879fd9c728a11633361b09ac89f5beff23ee21af
Nader elhaj <naderelhaj3@gmail.com>
2021-04-19 18:03:55 +0100

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
-
+import HeaderOption from './Header/HeaderOption/HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ChatIcon from '@material-ui/icons/Chat';
 function Header() {
     return (
         <div className="header">
@@ -12,7 +17,14 @@ function Header() {
                     <input type="text"/>
                 </div>
             </div>
-            <div className="header__right"></div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon}  title="Home" />
+                <HeaderOption  Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Chat"/>
+                <HeaderOption avatar="images/me.jpg" title="Me"/>
+            </div>
         </div>
     )
 }
