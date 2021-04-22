fa2a0d1b4f3144e5ce46e7614a5a62e2e4b1f502
Nader Elhaj <naderelhaj3@gmail.com>
2021-04-22 19:23:03 +0200

    Feed Input Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import SideBar from './components/SideBar/SideBar';
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className="app__body">
         <SideBar/>
+        <Feed />
       </div>
     </div>
   );
