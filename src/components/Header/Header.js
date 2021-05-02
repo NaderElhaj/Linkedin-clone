281619f6d0216e4ece2d6d59ba9223057439eb42
Nader Elhaj <naderelhaj3@gmail.com>
2021-05-02 05:26:59 +0200

    fixing warnings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import ChatIcon from "@material-ui/icons/Chat";
-import { useDispatch, useSelector } from "react-redux";
-import { logout, selectUser } from "../../features/userSlice";
+import { useDispatch } from "react-redux";
+import { logout } from "../../features/userSlice";
 import { auth } from "../../firebase";
 function Header() {
   const dispatch = useDispatch();
